953a42161d3713d2ef5f6bc58cfecfe03c3773b8
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-05-27 14:42:55 -0300

    FEAT: (Requirement 04 / Archetypes) Created the Archetype class with proposed attributes and methods.

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(
+    private _name: string,
+    special = 0,
+    cost = 0,
+  ) {
+    this._special = special;
+    this._cost = cost;
+  }
+
+  get name(): string { return this._name; }
+  get special(): number { return this._special; }
+  get cost(): number { return this._cost; }
+
+  abstract get energyType(): EnergyType;
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented'); 
+  }
+}
